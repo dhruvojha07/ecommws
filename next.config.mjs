9b26aa8cd6bcc12fa3424315dd6b0da45e37cfbf
Dhruv Ojha <dhruvojha007@gmail.com>
2024-06-22 11:07:00 +0530

    added air-bnb image slider

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                hostname: "localhost",
+                pathname: "**",
+                protocol: "http",
+            }
+        ]
+    }
 };
 
 export default nextConfig;
